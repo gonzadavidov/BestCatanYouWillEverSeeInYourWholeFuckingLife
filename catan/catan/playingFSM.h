411ced3619f30a40bc5683d0853ca6c422cc66f1
Federico Tondi <fedetondi@gmail.com>
2018-11-23 00:53:27 -0300

    charlar fsm de playing y los eventos que queremos recibir

--- catan/catan/playingFSM.h
+++ catan/catan/playingFSM.h
@@ -10,11 +10,11 @@ private:
 
 #define TX(x) (static_cast<void (genericFSM::* )(genericEvent *)>(&bossFSM::x)) //casteo a funcion, por visual
 
-	const fsmCell fsmTable[6][7] = {
-		//
-	{ { START_MENU,TX(sendInput) },{ WAITING_CONNECTION,TX(verdespues) },{ START_MENU,TX(error) },{ START_MENU,TX(end1) },{ START_MENU,TX(end2) },{ START_MENU,TX(nada) },{ START_MENU,TX(nada) } },			//MY_TURN
-	{ { WAITING_CONNECTION,TX(mandar) },{ HANDSHAKING,TX(pasarcomun) },{ START_MENU,TX(verdespues) },{ START_MENU,TX(verdsp) },{ WAITING_CONNECTION,TX(end2) },{ WAITING_CONNECTION,TX(fijarsesierror) },{ WAITING_CONNECTION,TX(refresh) } }, //OPPONENT_TURN
-	{ { HANDSHAKING,TX(mandar) },{ PLAYING,TX(verdespues) },{ START_MENU,TX(verdespues) },{ WAITING_TO_QUIT,TX(verdsp) },{ HANDSHAKING,TX(end2) },{ HANDSHAKING,TX(mandar) },{ HANDSHAKING,TX(mandar) } },			//BUILDING
+	const fsmCell fsmTable[5][5] = {
+		//offer_trade					build					validate_building							validate_oponent_trade														robber	
+	{ { EXCHANGE,TX(mandaroferta) },{ BUILDING,TX(construyo) },{ MY_TURN,TX(nada) },						{ MY_TURN,TX(NADA) },													{ MY_TURN,TX(sacarcartasymoverrobber) } },			//MY_TURN
+	{ { OPONENT_TURN,TX(nada) },	{ OPONENT_TURN,TX(nada) }, { OPONENT_TURN,TX(verificojugadaoponente) },{ OPONENT_EXCHANGE,TX(verificoelcambioysiesparamidecidosiaceptoonodsp) },{ OPONENT_TURN,TX(sacarcartas) },{ WAITING_CONNECTION,TX(fijarsesierror) },{ WAITING_CONNECTION,TX(refresh) } }, //OPPONENT_TURN
+	{ { ,TX(mandar) },{ PLAYING,TX(verdespues) },{ START_MENU,TX(verdespues) },{ WAITING_TO_QUIT,TX(verdsp) },{ HANDSHAKING,TX(end2) },{ HANDSHAKING,TX(mandar) },{ HANDSHAKING,TX(mandar) } },			//BUILDING
 	{ { PLAYING,TX(mandar) },{ REMATCH,TX(avisarquiengano) },{ START_MENU,TX(verdesp) },{ WAITING_TO_QUIT,TX(verdsp) },{ PLAYING,TX(end2) },{ PLAYING,TX(mandar) },{ PLAYING,TX(mandar) } },				//OPPONENT_EXCHANGE
 	{ { REMATCH,TX(mandar) },{ HANDSHAKING,TX(verdesp) },{ START_MENU,TX(verdesp) },{ WAITING_TO_QUIT,TX(mandogameover) },{ REMATCH,TX(end2) },{ REMATCH,TX(mandar) },{ REMATCH,TX(mandar) } }				//EXCHANGE
 	};
